ddffa6b14dcf8a23731c731e6e1457e27cb0cd8c
maria775 <32092072+maria775@users.noreply.github.com>
2017-12-01 14:47:06 +0300

    Recipes js added

---
+++ Backend/data/RecipesData.js
@@ -0,0 +1,71 @@
+var recipes_info = [
+    {
+        id:1,
+
+        icon: 'images/Kinderpunsch-4.jpg',
+        title: "Різдвяний пунш",
+        type: 'Напої',
+        subtype:'без алкоголю'
+    },
+   {
+        id:2,
+
+        icon: 'images/Gingerbread-Hot-Chocolate-6.jpg',
+        title: "Гарячий шоколад з імбирним пряником",
+        type: 'Напої',
+        subtype:'без алкоголю'
+    },
+       {
+        id:3,
+
+        icon: 'images/Hot-Chocolate-Healthy-Salted-Caramel-Flavor.jpg',
+        title: "Какао зі збитими вершками і карамеллю",
+        type: 'Напої',
+        subtype:'без алкоголю'
+    },
+       {
+        id:4,
+
+        icon: 'images/christmas-drinks.jpg',
+        title: "Пряний ег-ног зі згущеним молоком",
+        type: 'Напої',
+        subtype:'з алкоголем'
+    },
+        {
+        id:5,
+
+        icon: 'images/candied-orange-slices.jpg',
+        title: "Апельсинові цукерки з шоколадом",
+        type: 'Солодощі'
+    },
+    { id:6,
+
+        icon: 'images/Sugar-Cut-Out-Cookies-iStock_000010323956Medium-1024x682.jpg',
+        title: "Різдвяні імбирні пряники",
+        type: 'Солодощі'
+    },
+    { id:7,
+
+        icon: 'images/20121010-vegetarian-lasagna-spinach-mushroom-whole-thumb-625xauto-278006.jpg',
+        title: "Різдвяна лазанья зі шпинатом та грибами",
+        type: 'Страви',
+        subtype: 'вега'
+    },
+    {   id:8,
+
+        icon: 'images/christmas-decorated-soup-1.jpg',
+        title: "Різдвяний гарбузовий суп-пюре",
+        type: 'Страви',
+        subtype: 'вега'
+    },
+    {  id:9,
+
+        icon: 'images/Christmas tree salad.jpg',
+        title: "Святковий овочевий салат",
+        type: 'Страви',
+        subtype: 'вега'
+    }
+
+];
+
+module.exports = recipes_info;
